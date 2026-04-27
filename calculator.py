adc1a12537d652e5cd29f330052137ac5dc8a51a
waadkhalifa020-bit <waadkhalifa020@gmail.com>
2026-04-28 00:06:40 +0300

    Implement basic operations in simple calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Simple Calculator
+
+a = 10
+b = 5
+
+print("Number 1:", a)
+print("Number 2:", b)
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
